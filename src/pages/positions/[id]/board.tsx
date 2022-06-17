f8862e08c5f19602bc1a71da82abd57ea6e53df9
JuJarazo <julianjarazo94@gmail.com>
2022-06-17 20:39:06 -0300

    Add evaluation timeline

--- src/pages/positions/[id]/board.tsx
+++ src/pages/positions/[id]/board.tsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
+
+import { Candidate } from '@/components/board/boardCard';
 import BoardComponent from '@/components/board/boardComponent';
+import { ProfileDrawerComponent } from '@/components/board/profileDrawerComponent';
 import { Meta } from '@/layout/Meta';
 import { Main } from '@/templates/Main';
 
 const Board = () => {
+  const [openProfile, setOpenProfile] = useState(false);
+  const [selectedProfile, setSelectedProfile] = useState<Candidate>(null);
+
   return (
     <Main meta={<Meta title="Hirely" description="Your hiring buddy." />}>
       <h2 className="mb-10 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl">
         Front End Developer
       </h2>
-      <BoardComponent />
+      <BoardComponent
+        setOpen={setOpenProfile}
+        setSelectedProfile={setSelectedProfile}
+      />
+      <ProfileDrawerComponent
+        open={openProfile}
+        setOpen={setOpenProfile}
+        selectedProfile={selectedProfile}
+      />
     </Main>
   );
 };
